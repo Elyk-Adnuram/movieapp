380abf75ab0332408408370f71fccc9d8bdcd107
Kyle <kmarunda@gmail.com>
2022-05-26 10:13:29 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const config = require("./config/key");
 //mongoose is used to connnect to the database
 const mongoose = require("mongoose");
 const connect = mongoose
-  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.mongoURI || config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
